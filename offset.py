5f6311e6f520eeb180c85b9ad636c7acdf47770e
Nadhir <mamokaze@gmail.com>
2021-07-17 19:35:10 +0100

    Better folder structuring

--- offset.py
+++ offset.py
@@ -1,42 +1,45 @@
 import re
 from pathlib import Path
 from tqdm import tqdm
+import os
 
 print('\nParsing Offset')
 
 places = ['alger', 'adrar', 'djelfa']
 p = {'alger': 25, 'djelfa': 24, 'adrar': 16}
 
-for place in places:
-	Path(f'./output/{place}/offset').mkdir(parents=True, exist_ok=True)
+for dir in os.listdir('./pages'):
+	if (re.match(r'\d{4}', dir)):
+		for place in places:
+			Path(f'./output/{dir}/{place}/offset').mkdir(parents=True, exist_ok=True)
 
-	for i in tqdm(range(3, 15), f'  Parsing Offset ({place})'):
-		output = open(f'./output/{place}/offset/{i-2}.txt', 'w', encoding='utf-8')
+			for i in tqdm(range(3, 15), f'  Parsing Offset ({place})'):
+				output = open(f'./output/{dir}/{place}/offset/{i-2}.txt', 'w', encoding='utf-8')
 
-		file = open(f'./pages/{place}/offset/page-{i}.txt', 'r', encoding='utf-8')
+				file = open(f'./pages/{dir}/{place}/offset/page-{i}.txt', 'r', encoding='utf-8')
 
-		if place == 'adrar':
-			matches = tuple(re.finditer(r'(-?(\d+)\s{1,2}){96}', file.read()))
+				if place == 'adrar':
+					matches = tuple(re.finditer(r'(-?(\d+)\s{1,2}){96}', file.read()))
 
-			first = list(filter(None, matches[0].group().split(' ')))
-			first = [first[i:i + p[place]] for i in range(0, len(first), p[place])]
+					first = list(filter(None, matches[0].group().split(' ')))
+					first = [first[i:i + p[place]] for i in range(0, len(first), p[place])]
 
-			second = list(filter(None, matches[1].group().split(' ')))
-			second = [second[i:i + p[place]] for i in range(0, len(second), p[place])]
+					second = list(filter(None, matches[1].group().split(' ')))
+					second = [second[i:i + p[place]] for i in range(0, len(second), p[place])]
 
-			lines = ''
-			for a in first: lines += '\n' + '|'.join(a)
-			lines += '\n'
-			for a in second: lines += '\n' + '|'.join(a)
+					lines = ''
+					for a in first: lines += '\n' + '|'.join(a)
+					lines += '\n'
+					for a in second: lines += '\n' + '|'.join(a)
 
-		else:
-			matches = tuple(re.finditer(r'(-?(\d+)\s{1,2}){138,151}', file.read()))
+				else:
+					matches = tuple(re.finditer(r'(-?(\d+)\s{1,2}){138,151}', file.read()))
 
-			arr = list(filter(None, matches[0].group().split(' ')))
-			arr = [arr[i:i + p[place]] for i in range(0, len(arr), p[place])]
+					arr = list(filter(None, matches[0].group().split(' ')))
+					arr = [arr[i:i + p[place]] for i in range(0, len(arr), p[place])]
 
-			lines = ''
-			for a in arr:
-				if len(a) == p[place] : lines += '\n' + '|'.join(a)
-				
-		output.write(lines[1:])
+					lines = ''
+					for a in arr:
+						if len(a) == p[place] : lines += '\n' + '|'.join(a)
+						
+				output.write(lines[1:])
--- scrape.py
+++ scrape.py
@@ -2,29 +2,36 @@ import PyPDF4
 import fitz
 from pathlib import Path
 from tqdm import tqdm
+import os
+import re
+
+INPUT_FOLDER = './input'
 
 print('Scraping data from PDF')
 
-places = ['alger', 'adrar', 'djelfa']
+for dir in os.listdir(INPUT_FOLDER):
+	if (re.match(r'\d{4}', dir)):
+		p = os.path.join(INPUT_FOLDER, dir)
+		places = ['alger', 'adrar', 'djelfa']
 
-for place in places:
-	Path(f'./pages/{place}/timing').mkdir(parents=True, exist_ok=True)
-	Path(f'./pages/{place}/offset').mkdir(parents=True, exist_ok=True)
+		for place in places:
+			Path(f'./pages/{dir}/{place}/timing').mkdir(parents=True, exist_ok=True)
+			Path(f'./pages/{dir}/{place}/offset').mkdir(parents=True, exist_ok=True)
 
-	file = fitz.open(f'./1442/{place}.pdf')
+			file = fitz.open(f'{p}/{place}.pdf')
 
-	i = 1
-	for page in tqdm(file, f'  Scraping Timing ({place})'):
-		out = open(f'./pages/{place}/timing/page-{i}.txt', 'wb')
-		i += 1
-		out.write(page.getText().encode('utf-8'))
+			i = 1
+			for page in tqdm(file, f'  Scraping Timing ({place.capitalize()} - {dir})'):
+				out = open(f'./pages/{dir}/{place}/timing/page-{i}.txt', 'wb')
+				i += 1
+				out.write(page.getText().encode('utf-8'))
 
-	pdfFileObj = open(f'./1442/{place}.pdf', 'rb')
-	pdfReader = PyPDF4.PdfFileReader(pdfFileObj, strict=False)
+			pdfFileObj = open(f'{p}/{place}.pdf', 'rb')
+			pdfReader = PyPDF4.PdfFileReader(pdfFileObj, strict=False)
 
-	for i in tqdm(range(3, 15), f'  Scraping Offset ({place})'):
+			for i in tqdm(range(3, 15), f'  Scraping Offset ({place.capitalize()} - {dir})'):
 
-		pageObj = pdfReader.getPage(i - 1)
-		pages_text = pageObj.extractText()
+				pageObj = pdfReader.getPage(i - 1)
+				pages_text = pageObj.extractText()
 
-		open(f'./pages/{place}/offset/page-{i}.txt', 'w', encoding='utf-8').write(' '.join(pages_text.split('\n')))
+				open(f'./pages/{dir}/{place}/offset/page-{i}.txt', 'w', encoding='utf-8').write(' '.join(pages_text.split('\n')))
